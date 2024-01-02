1b87125fc62f5131b924cbe4fcd2da7feb844a76
Ygyuri <gideonyuri15@gmail.com>
2024-01-02 12:26:18 -0800

    add

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let n = reportWithIterator.next();
-  let res = '';
+  const employees = [];
 
-  while (!n.done) {
-    res += `${n.value} | `;
-    n = reportWithIterator.next();
+  // eslint-disable-next-line
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
   }
-  return res.slice(0, res.length - 3);
+
+  return employees.join(' | ');
 }
