ce93e0ac236449d5acee3d39de43f21820634113
Ygyuri <gideonyuri15@gmail.com>
2024-01-02 12:17:59 -0800

    9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,12 +1,15 @@
-import getBudgetObject from './7-geteBudgetObject';
+// eslint-disable-next-line
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
+    // eslint-disable-next-line
     getIncomeInDollars(income) {
       return `$${income}`;
     },
+    // eslint-disable-next-line
     getIncomeInEuros(income) {
       return `${income} euros`;
     },
