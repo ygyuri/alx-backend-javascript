5d8d4d99bbcdd38ea30b72f0bbc2a79cfb636814
ygyuri <gideonyuri15@gmail.com>
2024-02-29 09:36:34 +0300

    debug

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,7 @@
-module.exports = function calculateNumber(a, b = 0) {
-    const aNum = Number(a);
-    const bNum = Number(b);
-  
-    if (Number.isNaN(aNum) || Number.isNaN(bNum))
-      throw TypeError('Parameters must be numbers');
-  
-    return Math.round(aNum) + Math.round(bNum);
-  };
+function calculateNumber(a, b) {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+    return roundedA + roundedB;
+}
+
+module.exports = calculateNumber;
